b889a5de54edc4b0554ee234d762e373519d07a7
Michael Feeney <mfeeney@fanshawec.ca>
2019-09-05 16:10:53 -0400

    Changed to the glm math library and perspective projection

--- GDP2019_20/theMain.cpp
+++ GDP2019_20/theMain.cpp
@@ -1,6 +1,14 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "linmath.h"
+//#include "linmath.h"
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp>
+// glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/gtc/type_ptr.hpp> // glm::value_ptr
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -41,47 +49,57 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
+
+
 int main(void)
 {
 	GLFWwindow* window;
 	GLuint vertex_buffer, vertex_shader, fragment_shader, program;
 	GLint mvp_location, vpos_location, vcol_location;
+
 	glfwSetErrorCallback(error_callback);
 	if (!glfwInit())
+	{
 		exit(EXIT_FAILURE);
+	}
+
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
 	window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
+
 	glfwSetKeyCallback(window, key_callback);
 	glfwMakeContextCurrent(window);
-
-
-//	gladLoadGL(glfwGetProcAddress);
-	gladLoadGL();
-
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 	glfwSwapInterval(1);
+
 	// NOTE: OpenGL error checks have been omitted for brevity
 	glGenBuffers(1, &vertex_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
 	glCompileShader(vertex_shader);
+
 	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
 	glCompileShader(fragment_shader);
 	program = glCreateProgram();
+
 	glAttachShader(program, vertex_shader);
 	glAttachShader(program, fragment_shader);
 	glLinkProgram(program);
+
 	mvp_location = glGetUniformLocation(program, "MVP");
 	vpos_location = glGetAttribLocation(program, "vPos");
 	vcol_location = glGetAttribLocation(program, "vCol");
+
 	glEnableVertexAttribArray(vpos_location);
 	glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
 		sizeof(vertices[0]), (void*)0);
@@ -92,17 +110,49 @@ int main(void)
 	{
 		float ratio;
 		int width, height;
-		mat4x4 m, p, mvp;
+		//       mat4x4 m, p, mvp;
+		glm::mat4 m, p, v, mvp;
 		glfwGetFramebufferSize(window, &width, &height);
 		ratio = width / (float)height;
 		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
-		mat4x4_identity(m);
-		mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-		mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-		mat4x4_mul(mvp, p, m);
+
+		//         mat4x4_identity(m);
+		m = glm::mat4(1.0f);
+
+		//mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+		glm::mat4 rotateZ = glm::rotate(glm::mat4(1.0f),
+			(float)glfwGetTime(),
+			glm::vec3(0.0f, 0.0, 1.0f));
+
+		m = m * rotateZ;
+
+		//mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		p = glm::perspective(0.6f,
+			ratio,
+			0.1f,
+			1000.0f);
+
+		v = glm::mat4(1.0f);
+
+		glm::vec3 cameraEye = glm::vec3(0.0, 0.0, -4.0f);
+		glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f);
+
+		v = glm::lookAt(cameraEye,
+			cameraTarget,
+			upVector);
+
+		//mat4x4_mul(mvp, p, m);
+		mvp = p * v * m;
+
+
 		glUseProgram(program);
-		glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
+
+
+		//glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
+		glUniformMatrix4fv(mvp_location, 1, GL_FALSE, glm::value_ptr(mvp));
+
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
