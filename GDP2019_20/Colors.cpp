87f8ad6575cd6089a995194417a4b09fab9dca9b
Daniel Casado <jdanielca@gmail.com>
2019-10-15 19:02:30 -0400

    Adds pre defined vector colors.

---
+++ GDP2019_20/Colors.cpp
@@ -0,0 +1,11 @@
+#include "Colors.h"
+
+vec3 Colors::white = vec3(1.0f, 1.0f, 1.0f);
+vec3 Colors::grey = vec3(0.7f, 0.7f, 0.7f);
+vec3 Colors::black = vec3(1.0f, 1.0f, 1.0f);
+vec3 Colors::yellow = vec3(1.0f, 1.0f, 0.0f);
+vec3 Colors::blue = vec3(0.0f, 0.0f, 1.0f);
+vec3 Colors::purple = vec3(1.0f, 0.0f, 1.0f);
+vec3 Colors::orange = vec3(1.0f, 1.0f, 0.0f);
+vec3 Colors::green = vec3(0.0f, 1.0f, 0.0f);
+vec3 Colors::red = vec3(1.0f, 0.0f, 0.0f);
---
+++ GDP2019_20/Colors.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <glm/vec3.hpp>
+
+using namespace glm;
+
+class Colors {
+public:
+	static vec3 white, grey, black, yellow, blue, purple, orange, green, red;
+};
