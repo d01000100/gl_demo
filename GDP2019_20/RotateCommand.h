3edd3dbe72948f8543c7066a34544fe0f418c312
Daniel Casado <jdanielca@gmail.com>
2019-11-27 15:03:07 -0500

    Adds ease in RotateCommand.

--- GDP2019_20/RotateCommand.h
+++ GDP2019_20/RotateCommand.h
@@ -16,14 +16,19 @@ private:
 	glm::vec3 deltaEulerRotation,
 		eulerRotationVel,
 		currentEulerRotation;
-	float totalDuration, currentTime;
+	float totalDuration, currentTime,
+		easeInDuration, easeOutDuration,
+		easeOutStart,
+		t, maxVel;
 	aGameItem* gameItem;
 	bool isInitialized;
 public:
 	RotateCommand(
 		aGameItem* gameItem,
 		glm::vec3 deltaEulerRotation,
-		float totalDuration)
+		float totalDuration,
+		float easeInDuration = 0,
+		float easeOutDuration = 0)
 	{
 		// TODO: manage more than 180° turn rotation
 		this->gameItem = gameItem;
@@ -34,6 +39,16 @@ public:
 		this->eulerRotationVel = deltaEulerRotation / totalDuration;
 		this->nullQRotation = glm::quat(glm::vec3(0, 0, 0));
 		this->deltaQRotation = glm::quat(glm::radians(deltaEulerRotation));
+
+		this->easeInDuration = easeInDuration;
+		this->easeOutDuration = easeOutDuration;
+		t = 0;
+		this->easeOutStart = totalDuration - easeOutDuration;
+
+		float maxVelDuration = totalDuration - easeInDuration - easeOutDuration;
+		maxVelDuration = std::max(0.0001f, maxVelDuration);
+		// This was NOT trivial
+		this->maxVel = 1.0f / (maxVelDuration + easeInDuration / 2 + easeOutDuration / 2);
 
 		currentTime = 0.0f;
 		isInitialized = false;
@@ -56,11 +71,24 @@ public:
 
 		if (!isDone())
 		{
-			// Quaterion based rotation
-			float AmountOfRotationCompleted = currentTime / totalDuration;
+			float currentVel = 0;
+			if (currentTime < easeInDuration) // ease in
+			{
+				currentVel = maxVel * glm::smoothstep(0.0f, easeInDuration, currentTime);
+			}
+			else if (currentTime < easeOutStart) // just move
+			{
+				currentVel = maxVel;
+			}
+			else // ease Out
+			{
+				currentVel = maxVel * glm::smoothstep(totalDuration, easeOutStart, currentTime);
+			}
+			t += currentVel * deltaTime;
+
 			glm::quat qCurrentRotation
 				= glm::slerp(startOrientation, this->endOrientation,
-					AmountOfRotationCompleted);
+					t);
 
 			gameItem->setOrientation(qCurrentRotation);
 			//gameItem->addOrientation(eulerRotationVel * deltaTime);
--- GDP2019_20/ScriptBuilder.cpp
+++ GDP2019_20/ScriptBuilder.cpp
@@ -20,11 +20,12 @@ iCommand* TestScript()
 	//script->pushCommand(new MoveCommand(player, glm::vec3(10, 10, -10), glm::vec3(10, 10, -10), 0.00001f, 0.0f, 0.0f));
 	//script->pushCommand(new MoveCommand(ship, glm::vec3(0, 34, -20), glm::vec3(20, 30, 20), 5.0f, 1.0f, 1.5f));
 	//script->pushCommand(new MoveCommand(player, glm::vec3(10, 10, -10), glm::vec3(0, 20, 0), 2.0f, 0.0f, 1.0f));
-	allUp->addCommand(new RideCurve(ship1, glm::vec3(-50, 30, 0), glm::vec3(50, 30, 0), glm::vec3(0, 120, 0), 12, 4, 4));
-	allUp->addCommand(new RideCurve(ship2, glm::vec3(-50, 30, 30), glm::vec3(50, 30, 30), glm::vec3(0, 120, 0), 12, 0, 0));
+	//allUp->addCommand(new RideCurve(ship1, glm::vec3(-50, 30, 0), glm::vec3(50, 30, 0), glm::vec3(0, 120, 0), 12, 4, 4));
+	//allUp->addCommand(new RideCurve(ship2, glm::vec3(-50, 30, 30), glm::vec3(50, 30, 30), glm::vec3(0, 120, 0), 12, 0, 0));
 	//allUp->addCommand(new FollowCommand(player, ship, glm::vec3(0,10,0), 20, 30, 60, 0));
 	//allUp->addCommand(new MoveCommand(ship, ship->getPos() + glm::vec3(0, 50, 160), ship->getPos() - glm::vec3(0, 50, 160), 7, 2, 2));
-	//allUp->addCommand(new RotateCommand(ship, glm::vec3(0, 0, 90), 1.0));
+	allUp->addCommand(new RotateCommand(ship1, glm::vec3(0, 170, 0), 7.0, 2, 2));
+	allUp->addCommand(new RotateCommand(ship2, glm::vec3(0, 170, 0), 7.0));
 	//allUp->addCommand(new MoveCommand(player, glm::vec3(0, 20, 0), glm::vec3(0, 40, 0), 2.0f, 0.0f, 1.0f));
 	//allUp->addCommand(new MoveCommand(ship, glm::vec3(20, 30, 20), glm::vec3(20, 50, 20), 2.0f, 0.5f, 1.0f));
 	script->pushCommand(allUp);
