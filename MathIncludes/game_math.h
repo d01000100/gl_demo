e00b93a7e2db42e3770c45e3ff500bf875a273f0
Daniel Casado <jdanielca@gmail.com>
2020-01-30 02:21:19 -0500

    Adds a project to include the math dependencies.

---
+++ MathIncludes/game_math.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/vec3.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/gtx/projection.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <glm/mat4x4.hpp>
